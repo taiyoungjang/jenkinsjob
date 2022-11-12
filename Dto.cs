4e06dc94d68d48578fe34ebf27370ef77b407446
Taiyoung Jang <taiyoung.jang@gmail.com>
2022-11-12 13:13:34 +0900

    test code

---
+++ Dto.cs
@@ -0,0 +1,4 @@
+﻿
+public record RequestModalDto(string type, string value, long action_time, string message );
+
+public record SubmissionDto(string type, string actions, long action_time, string message, long value );
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using System.Net.Http.Headers;
 using System.Text;
-using System.Text.Json;
 using System.Text.Json.Nodes;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,26 +14,59 @@ var credentials = Encoding.ASCII.GetBytes($"{jenkinsUser}:{jenkinsToken}");
 AuthenticationHeaderValue header = new ("Basic", Convert.ToBase64String(credentials));
 httpClient.DefaultRequestHeaders.Authorization = header;
 
-app.MapGet("/build/{job}", (string job) => Build(job) );
-app.MapGet("/listjob", () => List() );
+app.MapGet("/build/{job}", Build );
+app.MapGet("/listjob", List );
+app.MapPost("/request_modal",  RequestModal );
+app.MapPost("/submission",  Submission );
+app.MapPost("/",  PostJson );
 
 app.Run();
 
-string Build(string job)
+async Task<IResult> PostJson(HttpRequest request)
+{
+    Stream stream = new MemoryStream();
+    StreamReader reader = new StreamReader(stream);
+    await request.BodyReader.CopyToAsync(stream);
+    stream.Position = 0;
+    string json = reader.ReadToEnd();
+    JsonNode jsonNode = JsonNode.Parse(json);
+    var type = jsonNode!["type"];
+    switch (type.GetValue<string>())
+    {
+        case "request_modal":
+            var requestModalDto = System.Text.Json.JsonSerializer.Deserialize<RequestModalDto>(json);
+            return RequestModal(requestModalDto);
+        case "submission":
+            var submissionDto = System.Text.Json.JsonSerializer.Deserialize<SubmissionDto>(json);
+            return Submission(submissionDto);
+    }
+    return Results.NotFound();
+}
+
+IResult RequestModal(RequestModalDto dto)
+{
+    return Results.Json(dto.message);
+}
+IResult Submission(SubmissionDto dto)
+{
+    return Results.Json(dto.message);
+}
+
+IResult Build(string job)
 {
     if (string.IsNullOrEmpty(job))
     {
-        return "invalid job";
+        return Results.Json(new { message = "invalid job" });
     }
     using HttpRequestMessage requestMessage = new HttpRequestMessage();
     requestMessage.Method = HttpMethod.Post;
     requestMessage.RequestUri = new Uri($"{jenkinsUrl}/job/{job}/build");
     using var response =  httpClient.Send(requestMessage);
     var statusCode = new { response.StatusCode };
-    return statusCode.ToString()!;
+    return Results.Json(statusCode);
 }
 
-string List()
+IResult List()
 {
     using HttpRequestMessage requestMessage = new HttpRequestMessage();
     requestMessage.Method = HttpMethod.Post;
@@ -47,6 +79,5 @@ string List()
         jobs.Add( job!["name"]!.ToString() );
     }
 
-    var ret = JsonSerializer.Serialize( new { jobs });
-    return ret;
+    return Results.Json(jobs);
 }
