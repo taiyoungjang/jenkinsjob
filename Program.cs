14b9cb09eaabb81db625fdb5254a39892687ddc5
Taiyoung Jang <taiyoung.jang@gmail.com>
2022-11-12 11:27:43 +0900

    job list

--- Program.cs
+++ Program.cs
@@ -8,23 +8,36 @@ var jenkinsUrl = builder.Configuration.GetValue<string>("jenkins:url");
 var jenkinsUser = builder.Configuration.GetValue<string>("jenkins:user");
 var jenkinsToken = builder.Configuration.GetValue<string>("jenkins:token");
 
-app.MapGet("/{job}", (string job) => JobCall(job) );
+HttpClient httpClient = new ();
+var credentials = Encoding.ASCII.GetBytes($"{jenkinsUser}:{jenkinsToken}");
+AuthenticationHeaderValue header = new ("Basic", Convert.ToBase64String(credentials));
+httpClient.DefaultRequestHeaders.Authorization = header;
+
+app.MapGet("/build/{job}", (string job) => Build(job) );
+app.MapGet("/listjob", () => List() );
 
 app.Run();
 
-string JobCall(string job)
+string Build(string job)
 {
     if (string.IsNullOrEmpty(job))
     {
         return "invalid job";
     }
-    using HttpClient httpClient = new ();
-    var credentials = Encoding.ASCII.GetBytes($"{jenkinsUser}:{jenkinsToken}");
-    AuthenticationHeaderValue header = new ("Basic", Convert.ToBase64String(credentials));
-    httpClient.DefaultRequestHeaders.Authorization = header;
     using HttpRequestMessage requestMessage = new HttpRequestMessage();
     requestMessage.Method = HttpMethod.Post;
     requestMessage.RequestUri = new Uri($"{jenkinsUrl}/job/{job}/build");
     using var response =  httpClient.Send(requestMessage);
-    return response.StatusCode.ToString();
+    var statusCode = new { response.StatusCode };
+    return statusCode.ToString()!;
+}
+
+string List()
+{
+    using HttpRequestMessage requestMessage = new HttpRequestMessage();
+    requestMessage.Method = HttpMethod.Post;
+    requestMessage.RequestUri = new Uri($"{jenkinsUrl}/api/json/json?tree=jobs[name,buildable,jobs[name,buildable,jobs[name,buildable]]]&pretty");
+    using var response =  httpClient.Send(requestMessage);
+    var result = response.Content.ReadAsStringAsync().Result;
+    return result;
 }
